cd9b255155b6d41da2367069338db3306a2ce6f7
unsliced <unsliced@gmail.com>
2017-10-23 17:41:43 +0100

    Initial commit for the PRI-based BROE2 Head project

---
+++ head-pri/Head2.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using Xunit;
+using Head2;
+
+namespace Head2.Tests
+{
+    public class Head2_Should
+    {
+        readonly Head2 _head2;
+
+        public Head2_Should()
+        {
+            _head2 = new Head2();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var result = _head2.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(6)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            var result = _head2.IsPrime(value);
+            
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void ReturnTrueGivenValuesLessThan10(int value)
+        {
+            var result = _head2.IsPrime(value);
+            
+            Assert.True(result, $"{value} should be prime");
+        }
+
+    }
+}
---
+++ head-pri/Head2/Head2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Head2
+{
+    public class Head2
+    {
+        public bool IsPrime(int candidate)
+        {
+            if(candidate <= 1)
+                return false;
+            if(candidate == 2)
+                return true;
+            
+            throw new NotImplementedException("can only work for values of 2 or lower");
+        }
+    }
+}
