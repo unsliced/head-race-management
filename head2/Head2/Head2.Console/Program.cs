4d5a3e11767b9adcd85bb1d907cc43d9737a7247
Chris Harrison <unsliced@gmail.com>
2019-10-23 21:49:46 +0100

    data deletion

---
+++ head2/Head2/Head2.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Head2.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
